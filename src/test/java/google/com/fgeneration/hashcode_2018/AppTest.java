e3ab40135e5c5b7a8a00d9a9b2c5897387d4ef50
giorgio <giopag27@gmail.com>
2018-02-26 19:07:08 +0100

    add before statement inside test class

--- src/test/java/google/com/fgeneration/hashcode_2018/AppTest.java
+++ src/test/java/google/com/fgeneration/hashcode_2018/AppTest.java
@@ -6,6 +6,7 @@
 package google.com.fgeneration.hashcode_2018;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -14,16 +15,23 @@ import org.junit.Test;
  */
 public class AppTest {
   private static String fieldString;
+  private static int fieldInt;
 
   @BeforeClass
   public static void bringUp() {
     fieldString = "string";
-
+  }
+  
+  @Before
+  public void eachTest() {
+	fieldInt = 3;
   }
 
   @Test
   public void testScratch() {
     Assert.assertTrue(fieldString.length() == 6);
     Assert.assertEquals(fieldString, "string");
+    Assert.assertNotNull(fieldInt);
+    Assert.assertEquals(fieldInt, 3);
   }
 }
