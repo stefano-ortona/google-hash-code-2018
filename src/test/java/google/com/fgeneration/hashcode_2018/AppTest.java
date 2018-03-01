0853dc3c86ea50ae7caec151266f85a9e9092d69
giorgio <giopag27@gmail.com>
2018-03-01 19:59:44 +0100

    first version  of test

--- src/test/java/google/com/fgeneration/hashcode_2018/AppTest.java
+++ src/test/java/google/com/fgeneration/hashcode_2018/AppTest.java
@@ -5,48 +5,79 @@
  */
 package google.com.fgeneration.hashcode_2018;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import google.com.fgeneration.hashcode_2018.logic.AssignDriverLogic;
+import google.com.fgeneration.hashcode_2018.logic.DefaultScoreRide;
+import google.com.fgeneration.hashcode_2018.model.CityStatus;
+import google.com.fgeneration.hashcode_2018.model.Driver;
+import google.com.fgeneration.hashcode_2018.model.Intersection;
+import google.com.fgeneration.hashcode_2018.model.Ride;
+
 /**
  * Unit test
  */
 public class AppTest {
-  private static String fieldString;
-  private static int fieldInt;
+  private static AssignDriverLogic logic;
 
   @BeforeClass
   public static void bringUp() {
-    fieldString = "string";
+	logic = new AssignDriverLogic();
   }
   
   @Before
   public void eachTest() {
-	fieldInt = 3;
+	
   }
 
   @Test
-  public void testScratch() {
-    Assert.assertTrue(fieldString.length() == 6);
-    Assert.assertEquals(fieldString, "string");
-    Assert.assertNotNull(fieldInt);
-    Assert.assertEquals(fieldInt, 3);
+  public void testExample() {
+	  // create drivers
+	  Driver driver0 = new Driver(0);
+	  Driver driver1 = new Driver(1);
+	  List<Driver> drivers = new ArrayList<Driver>();
+	  drivers.add(driver0);
+	  drivers.add(driver1);
+	  // create riders
+	  Ride ride0 = new Ride(0, new Intersection(0, 0), new Intersection(3, 2), 2, 9);
+	  Ride ride1 = new Ride(1, new Intersection(2, 1), new Intersection(0, 0), 0, 9);
+	  Ride ride2 = new Ride(1, new Intersection(0, 2), new Intersection(2, 0), 0, 9);
+	  List<Ride> rides = new ArrayList<Ride>();
+	  rides.add(ride0);
+	  rides.add(ride1);
+	  rides.add(ride2);
+	  // create city status
+	  CityStatus status = new CityStatus(new DefaultScoreRide(), 2);
+	  status.setDrivers(drivers);
+	  status.setRides(rides);
+	  status.setMaxTime(10);
+	  
+      // get the output
+	  Map<Driver, List<Ride>> output = logic.assignAllRides(status);
+	  
+	  // check the output
+	  for(Driver d:output.keySet()) {
+		List<Ride> driver_rides = output.get(d);
+		System.out.print("Driver ");
+		System.out.println(d);
+		System.out.println("");
+		for(Ride r:driver_rides) {
+			System.out.print("Rides: ");
+			System.out.println(r);
+		}
+		System.out.println("-----------------------------");
+		System.out.println("-----------------------------");
+	  }
+	  
   }
-  
-  @Test
-  public void testStringList() {
-	List<String> actual = Arrays.asList("a", "b", "c");
-    List<String> expected = Arrays.asList("a", "b", "c");
-	
-    Assert.assertEquals(expected, actual);
-    Assert.assertTrue(actual.contains(new String("a")));
-    Assert.assertEquals(new String("a"),actual.get(0));
-    Assert.assertNotEquals('a',actual.get(0));
-    Assert.assertFalse(actual.isEmpty());
-  }
+
 }
