d8d6b803d8ec66813cd3b91bdb2d5da5bedf4a62
Carlo Luchessa <c.luchessa@reply.it>
2018-03-01 20:20:06 +0100

    up.

--- src/main/java/google/com/fgeneration/hashcode_2018/logic/DefaultScoreRide.java
+++ src/main/java/google/com/fgeneration/hashcode_2018/logic/DefaultScoreRide.java
@@ -6,22 +6,11 @@ import google.com.fgeneration.hashcode_2018.model.Ride;
 import google.com.fgeneration.hashcode_2018.utils.Utils;
 
 public class DefaultScoreRide extends BestScoreRide {
-	
-	private static double DISTANCE_WEIGHT = 1.0;
-	private static double RIDE_LENGHT_WEIGHT = 1.0;
-	private static double BONUS_WEIGHT = 1.0;
 
 	@Override
 	public double getScore(Driver driver, Ride ride, CityStatus status) {
-		double distanceScore = DISTANCE_WEIGHT * Utils.getDistance(driver.getLastPositon(),  ride.getStart());
-
-		double rideLength = RIDE_LENGHT_WEIGHT * Utils.getDistance(ride.getStart(), ride.getEnd());
-
-		double bonus = BONUS_WEIGHT * (Utils.gotBonusForRide(driver, ride) ? status.getBonus() : 0);
-		
-		return rideLength 
-				+ bonus
-				- distanceScore;
+		double distanceScore = Utils.getDistance(driver.getLastPositon(),  ride.getStart());
+		return distanceScore;
 	}
 
 }
---
+++ src/main/java/google/com/fgeneration/hashcode_2018/logic/NearestScoreRide.java
@@ -0,0 +1,27 @@
+package google.com.fgeneration.hashcode_2018.logic;
+
+import google.com.fgeneration.hashcode_2018.model.CityStatus;
+import google.com.fgeneration.hashcode_2018.model.Driver;
+import google.com.fgeneration.hashcode_2018.model.Ride;
+import google.com.fgeneration.hashcode_2018.utils.Utils;
+
+public class NearestScoreRide extends BestScoreRide {
+	
+	private static double DISTANCE_WEIGHT = 1.0;
+	private static double RIDE_LENGHT_WEIGHT = 1.0;
+	private static double BONUS_WEIGHT = 1.0;
+
+	@Override
+	public double getScore(Driver driver, Ride ride, CityStatus status) {
+		double distanceScore = DISTANCE_WEIGHT * Utils.getDistance(driver.getLastPositon(),  ride.getStart());
+
+		double rideLength = RIDE_LENGHT_WEIGHT * Utils.getDistance(ride.getStart(), ride.getEnd());
+
+		double bonus = BONUS_WEIGHT * (Utils.gotBonusForRide(driver, ride) ? status.getBonus() : 0);
+		
+		return rideLength 
+				+ bonus
+				- distanceScore;
+	}
+
+}
