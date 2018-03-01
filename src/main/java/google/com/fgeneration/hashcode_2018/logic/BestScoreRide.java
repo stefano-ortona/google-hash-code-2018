f3d41aea5d2ca3a323b75e0d815a8cbb01d9385b
Carlo Luchessa <c.luchessa@reply.it>
2018-03-01 19:38:40 +0100

    up.

--- src/main/java/google/com/fgeneration/hashcode_2018/logic/BestScoreRide.java
+++ src/main/java/google/com/fgeneration/hashcode_2018/logic/BestScoreRide.java
@@ -1,11 +1,29 @@
 package google.com.fgeneration.hashcode_2018.logic;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import google.com.fgeneration.hashcode_2018.model.CityStatus;
 import google.com.fgeneration.hashcode_2018.model.Driver;
 import google.com.fgeneration.hashcode_2018.model.Ride;
 
-public interface BestScoreRide {
+public abstract class BestScoreRide {
 
-  public Ride getBestRide(Driver driver, CityStatus status);
+	public Ride getBestRide(Driver driver, CityStatus status) {
+		// iterare tutti i ride e calcolare lo score, scegliere best
+		// if condizione validità errata allora score -1
+		Map<Integer, Driver> scoreMap = new HashMap<>();
+		for (Ride ride : status.getRides()) {
+			
+		}
+		
+		return null;
+	}
+
+	private boolean isValid(Driver driver, Ride ride) {
+		return true;
+	}
+
+	public abstract int getScore(Driver driver, Ride ride, CityStatus status);
 
 }
