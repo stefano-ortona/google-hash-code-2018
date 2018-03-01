92308f31c440dfcc230279fb12e240ea211ca22b
Carlo Luchessa <c.luchessa@reply.it>
2018-03-01 19:49:59 +0100

    up.

---
+++ src/main/java/google/com/fgeneration/hashcode_2018/logic/DefaultScoreRide.java
@@ -0,0 +1,15 @@
+package google.com.fgeneration.hashcode_2018.logic;
+
+import google.com.fgeneration.hashcode_2018.model.CityStatus;
+import google.com.fgeneration.hashcode_2018.model.Driver;
+import google.com.fgeneration.hashcode_2018.model.Ride;
+
+public class DefaultScoreRide extends BestScoreRide {
+
+	@Override
+	public int getScore(Driver driver, Ride ride, CityStatus status) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
